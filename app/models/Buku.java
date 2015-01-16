bd9ee4ce43e7454928407ee68b127e31106a6bd2
arifadlileo <arifadlileo@gmail.com>
2015-01-15 19:33:21 -0800

    Future Master Ari Fadli

--- app/models/Buku.java
+++ app/models/Buku.java
@@ -1,5 +1,14 @@
 package models;
 
+import java.util.Date;
+
 public class Buku {
+	//@Required
+		public String isbn;
+		//@Required
+		public String judulbuku;
+		//@Required
+		public String penerbit;
+		public String pengarang;
 
 }
